1348c133888c7dc13c3838a6c99eff71449ea0d9
bhseo <ktseo41@gmail.com>
2019-10-15 20:40:18 +0900

    refactor: update homeitem component
    airbnb home-item component 인자 수정
    객체로 옵션을 받아서 유연하도록 수정

--- src/App.js
+++ src/App.js
@@ -11,7 +11,28 @@ function App() {
   const buttonHomeType = Button('숙소 인원', 'button-home-type');
   const buttonPrice = Button('가격', 'button-price');
   const buttonAddFilter = Button('필터 추가하기', 'button-add-filter');
-  const homeItem = HomeItem('home-1');
+  const homeItem = HomeItem({
+    className:'home-1', 
+    imgSrc:"https://a0.muscache.com/im/pictures/7451e3fb-d4ce-49f5-8be6-8d8abea79880.jpg",
+    homeType:"아파트 전체",
+    homeName:"apartment in sunset valley of sims world",
+    homeSpec:"인원 3명 · 원룸 · 침대 1개 · 욕실 1개",
+    roomPrice:"100,000",
+    roomFullPrice:"300,000",
+    starringScore:"5.0",
+    starringReviews:"12"
+  });
+  const homeItem2 = HomeItem({
+    className:"home-2",
+    imgSrc:"https://a0.muscache.com/im/pictures/37c5e0ec-048d-4a0b-b45b-ff27074ab632.jpg",
+    homeType:"아파트의 개인실",
+    homeName:"Manhattan Ave Apartment 2 stops from Times Square",
+    homeSpec:"인원 4명 · 침실 2개 · 침대 2개 · 욕실 2개",
+    roomPrice:"139,120",
+    roomFullPrice:"392,360",
+    starringScore:"4.6",
+    starringReviews:"32"
+  })
   return (
     <div className="App">
       <header className="App-header">
@@ -31,6 +52,7 @@ function App() {
       <h3>숙소 000개</h3>
         <div className="home-container">
           {homeItem}
+          {homeItem2}
         </div>
     </div>
   );
--- src/component/homeitem.js
+++ src/component/homeitem.js
@@ -1,42 +1,49 @@
 import React from 'react';
 import star from './star';
 
-const HomeItem = (className) => {
+/**
+ * 
+ * @param {*} homeSpec 
+ */
+const HomeItem = (home) => {
+    const { 
+        className, 
+        imgSrc, 
+        homeType, 
+        homeName, 
+        homeSpec, 
+        roomPrice, 
+        roomFullPrice, 
+        starringScore,
+        starringReviews
+    } = home;
     const classNames = `home-item d-flex flex-row ${className}`;
     return (
         <div className={classNames}>
             <div className="home-img-box">
-                <img width="300" height="200" src="https://a0.muscache.com/im/pictures/7451e3fb-d4ce-49f5-8be6-8d8abea79880.jpg"></img>
+                <img width="300" height="200" src={imgSrc}></img>
             </div>
             <div className="home-spec-box position-relative">
                 <div className="home-spec-inner-box">
                     <div className="spec-inner-upper-box d-flex flex-column">
-                        <div className="home-type">아파트 전체</div>
-                        <div className="home-name">sunset valley</div>
-                        <div className="home-spec">
-                            인원 3명
-                            · 
-                            원룸
-                            · 
-                            침대 1개
-                            · 
-                            욕실 1개
-                        </div>
+                        <div className="home-type">{homeType}</div>
+                        <div className="home-name">{homeName}</div>
+                        <div className="home-spec">{homeSpec}</div>
                     </div>
                     <div className="spec-inner-lower-left-box position-absolute d-flex align-items-center">
                         <div className="starring d-flex align-items-center">
                             {star}
                         </div>
                         <div className="starring-score">
-                            5.0
+                            {starringScore}
                         </div>
                         <div className="starring-reviews">
-                            (12)
+                            ({starringReviews})
                         </div>
                     </div>
                     <div className="spec-inner-lower-right-box position-absolute d-flex flex-column">
-                        <div className="room-price">₩100,000/박</div>
-                        <div className="room-full-price">총 요금 : ₩300,000</div>
+                        <div className="room-price">₩{roomPrice}/박</div>
+                        <div className="room-full-price">총 요금 : ₩{roomFullPrice}</div>
                     </div>
                 </div>
             </div>
