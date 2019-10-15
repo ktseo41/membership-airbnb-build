305dc3e5df6b283f1914de5a7394c647d4681767
bhseo <ktseo41@gmail.com>
2019-10-15 20:22:15 +0900

    feat(add): airbnb basic layout
    react이용 airbnb 기본 레이아웃 구현 및 업로드

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './component/logo';
 import './App.css';
+import Button from './component/button';
+import HomeItem from './component/homeitem';
 
 function App() {
   const name = 'membership-airbnb-build from ss38'
+  const buttonDate = Button('날짜', 'button-date');
+  const buttonPeople = Button('인원', 'button-people');
+  const buttonHomeType = Button('숙소 인원', 'button-home-type');
+  const buttonPrice = Button('가격', 'button-price');
+  const buttonAddFilter = Button('필터 추가하기', 'button-add-filter');
+  const homeItem = HomeItem('home-1');
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React Page : {name}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Buttons d-flex align-items-center">
+        {logo}
+        {buttonDate}
+        {buttonPeople}
+        {buttonHomeType}
+        {buttonPrice}
+        {buttonAddFilter}
+        </div>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
+      <p>
+          React Page : {name}
+      </p>
+      <h3>숙소 000개</h3>
+        <div className="home-container">
+          {homeItem}
+        </div>
     </div>
   );
 }
