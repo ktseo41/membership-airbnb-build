1018f7327253397c42cc65906c66b1368ed5674f
bhseo <ktseo41@gmail.com>
2019-10-25 02:12:17 +0900

    refactor: homesapp

--- src/container/homesApp.js
+++ src/container/homesApp.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import queries from '../query';
 import Home from '../component/home';
 import Count from '../component/homecount';
+import { GlobalContext } from '../App';
 
 const HomesApp = (props) => {
+    const globalContext = useContext(GlobalContext);
+    const [homeFilterState, setHomeFilterState] = useState([]);
+    globalContext.homeFilterState = homeFilterState;
+    globalContext.setHomeFilterState = setHomeFilterState;
+    console.log(globalContext);
     const { GET_ALL_HOME } = queries;
     const { loading, error, data } = useQuery(GET_ALL_HOME);
 
@@ -13,6 +19,7 @@ const HomesApp = (props) => {
         return <div>Loading...</div>;
     }
     if (error) {
+        console.error(error);
         return <div>Error Occured!</div>;
     }
     const { getAllHome } = data;
